ce9e8a5310032bda8b4c0a45e606afb1c15a37e1
kazuma313 <koerzoelmatondang@gmail.com>
2024-09-15 17:53:52 +0700

    UIchatbot already running with streamlit, but haven't using RAG

--- test/chat_UI.py
+++ test/chat_UI.py
@@ -1,6 +1,24 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import streamlit as st
+from langchain_openai.chat_models import ChatOpenAI
+
+st.title("🦜🔗 Quickstart App")
+
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
 
-if __name__ == '__main__':
-    print_hi('Python')
+def stream_response(input_text):
+    model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
+    for chunk in model.stream(input_text):
+        yield chunk.content
+
+
+with st.form("my_form"):
+    text = st.text_area(
+        "Enter text:",
+        "What are the three key pieces of advice for learning how to code?",
+    )
+    submitted = st.form_submit_button("Submit")
+    if not openai_api_key.startswith("sk-"):
+        st.warning("Please enter your OpenAI API key!", icon="⚠")
+    if submitted and openai_api_key.startswith("sk-"):
+        st.write_stream(stream_response(text))
