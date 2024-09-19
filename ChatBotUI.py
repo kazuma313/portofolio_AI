c352c0610726f99a31a855b3c326c7f6f00d8a88
kazuma313 <koerzoelmatondang@gmail.com>
2024-09-19 23:59:09 +0700

    add assets for show, and make sure all assets can be read in streamlit

--- ChatBotUI.py
+++ ChatBotUI.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import cv2
 # from langchain_openai.chat_models import ChatOpenAI
+from streamlit_pdf_viewer import pdf_viewer
 
 from langchain_openai import OpenAIEmbeddings, OpenAI
 from langchain_community.vectorstores import FAISS
@@ -10,13 +12,13 @@ from langchain_core.prompts import PromptTemplate
 
 from dotenv import load_dotenv
 
-project = ["Computer Vision", "Natural Language Processing", "Data Science"]
+project = ["Computer Vision", "Natural Language Processing", "Data Science", "Certificate"]
 load_dotenv()
 
 st.title("Kurnia Zulda Matondang")
 
 st.sidebar.header("#__My Project__")
-select_project = st.sidebar.selectbox('Project name', project)
+select_project = st.sidebar.selectbox("Project name", project)
 
 # openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 embedding = OpenAIEmbeddings(model="text-embedding-3-small")
@@ -29,7 +31,8 @@ new_vectorstore = FAISS.load_local(
 
 def stream_response(input_text):
     retrival_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-    prompt = PromptTemplate.from_template("""
+    prompt = PromptTemplate.from_template(
+        """
     Kamu merupakan seorang yang ingin melamar pekerjaan pada suatu perushaan. \
     berikan branding yang baik agar dapat meyakinkan Human Resource Development.\
     jika terdapat pertanyaan yang tidak sesuai dengan kompetensi atau bukan mengenai suatu perkerjaan,\
@@ -40,10 +43,9 @@ def stream_response(input_text):
     </context>
 
     {input}
-    """)
-    combine_doc_chain = create_stuff_documents_chain(
-        OpenAI(), prompt
+    """
     )
+    combine_doc_chain = create_stuff_documents_chain(OpenAI(), prompt)
     retrival_chain = create_retrieval_chain(
         new_vectorstore.as_retriever(), combine_doc_chain
     )
@@ -51,6 +53,7 @@ def stream_response(input_text):
         if "answer" in chunk:
             yield chunk["answer"]
         pass
+
 
 if select_project == project[1]:
     st.header("Chat bot about me")
@@ -66,3 +69,63 @@ if select_project == project[1]:
         #     st.warning("Please enter your OpenAI API key!", icon="⚠")
         # if submitted and openai_api_key.startswith("sk-"):
         #     st.write_stream(stream_response(text))
+
+elif select_project == project[0]:
+    st.header("My last computer vision project")
+    st.title("Multiple Cameras")
+    # col1, col2 = st.columns((1, 1))
+    # video_1 = col1.empty()
+    # video_2 = col2.empty()
+    #
+    # cap1 = cv2.VideoCapture("assets/videos/2024_01_23_18_43_00___Trim.mp4")
+    # cap2 = cv2.VideoCapture("assets/videos/face-mask-video.mp4")
+    #
+    # while cap1.isOpened() or cap2.isOpened():
+    #     ret1, frame1 = cap1.read()
+    #     ret2, frame2 = cap2.read()
+    #
+    #     frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+    #     frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB)
+    #     video_1.image(frame1, channels="RGB")
+    #     video_2.image(frame2, channels="RGB")
+
+    # with col1:
+    #     # video_file = open("assets/videos/face-mask-video.mp4", "rb")
+    #     # video_bytes = video_file.read()
+    #     st.video("assets/videos/face-mask-video.mp4", format="video/mp4")
+    # with col2:
+    #     video_file = open("assets/videos/2024_01_23_18_43_00___Trim.mp4", "rb")
+    #     video_bytes = video_file.read()
+    #     st.video(video_bytes)
+
+    col3, col4 = st.columns(2)
+
+    with col3:
+        st.header("Age detection")
+        st.image("assets/images/age_detection.jpg")
+
+    with col4:
+        pdf_viewer("assets/documents/Certificate of Completion Mastering AI Bootcamp - Kurnia Zulda Matondang.pdf")
+
+
+if select_project == project[-1]:
+    st.header("this is just a small part of what i learned which i show")
+    st.title("##AI Bootcamp certification")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.text("Got Excellent result")
+        pdf_viewer("assets/documents/Certificate of Excellence Mastering AI Bootcamp - Kurnia Zulda Matondang.pdf")
+
+    with col2:
+        st.text("Complete Bootcamp successfully")
+        pdf_viewer("assets/documents/Certificate of Completion Mastering AI Bootcamp - Kurnia Zulda Matondang.pdf")
+
+    st.title("## Udemy certification")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.text("Got Excellent result")
+        pdf_viewer("assets/documents/Udemy_COMVIS_KurniaZuldaMatondang.pdf")
+
+    with col2:
+        st.text("Complete Bootcamp successfully")
+        pdf_viewer("assets/documents/CV_Kurnia_Zulda_Matondang.pdf")
